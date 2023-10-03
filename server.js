5e6229731b0f1c21a1ca573d37a2ffa4b3b14ad2
Daniel Janusch <djanusch2007@gmail.com>
2023-10-03 05:11:57 -0700

    update server.js to allow POST
    also:
    - change some files to only be accessible from a referrer inside the website
    - add a middleware function to help with logging.
    - allow /home, /index, /upload, /download, and /delete. they redirect to the html page.

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const
 	https = require("https"),
 	app = express(),
 
-
 	// openssl req -nodes -x509 -days 730 -newkey rsa:4096 -keyout server.key -out server.crt
 	privateKey = fs.readFileSync("server.key", "utf-8"),
 	certificate = fs.readFileSync("server.crt", "utf-8"),
 	credentials = { key: privateKey, cert: certificate },
 
 
-	allowedMethods = "GET,OPTIONS",
+	allowedMethods = "GET,POST,OPTIONS",
 
+	ROOT = __dirname + "/public",
 	HTTPS_PORT = 443,
 	HTTP_PORT = 80,
 	IP = "127.0.0.1",
@@ -22,7 +22,18 @@ const
 		`https://${IP}:${HTTPS_PORT}`, `https://localhost:${HTTPS_PORT}`,
 		`http://${IP}:${HTTP_PORT}`, `http://localhost:${HTTP_PORT}`,
 	],
-	allowedGetLocations = ["/index.html", "/upload.html", "/robots.txt", "/favicon.ico"],
+	allowedGetAddressLocations = [
+		"/index.html",
+		"/upload.html",
+		"/robots.txt",
+	],
+	allowedGetMiscellaneousLocations = [
+		"/styles.css",
+		"/favicon.ico",
+		"/error-template.html",
+		"/results.html",
+	],
+	allowedGetLocations = allowedGetAddressLocations.concat(allowedGetMiscellaneousLocations),
 	allowedGetLocationsString = `Allowed paths for GET: ${
 		["/"].concat(allowedGetLocations).map(e => `'${e}'`).join(", ")
 	}\n`,
@@ -39,64 +50,143 @@ app.use(function setOrigin(req, res, next) {
 			allowedOrigins[0]
 	)
 
-	res.setHeader("Access-Control-Allow-Methods", allowedMethods);
+	res.setHeader("Access-Control-Allow-Methods", allowedMethods)
 
 	res.setHeader("Access-Control-Allow-Credentials", "true")
 
 	next()
 })
 
-function failurePage(method, loc, msg = "Invalid Request", linkHome = true) {
-	if (typeof loc !== "string")
-		loc = loc.url; // assume it is an express request object thing
 
-	try {
-		return fs.readFileSync("./404-template.html", "utf-8")
-			.replace("{method}", method)
-			.replace("{location}", loc)
-			.replace("{message}", msg)
-			.replace("{link}", linkHome ? '<a href="/">return to homepage</a>' : "")
-	} catch {
-		return msg ?? "Invalid Request";
+app.use(function (req, res, next) {
+	req.port = req.connection.remotePort
+	req.ipport = `${req.ip}:${req.port}`
+	req.referrer = req.get("referrer") ?? "direct address"
+	req.directAddress = req.referrer === "direct address"
+
+	req.logRequest =
+	res.logRequest = function logRequest(
+		returncode = 200,
+		message = "",
+		newline = true,
+	) {
+		if (typeof returncode === "object") {
+			if (returncode.ret != null)
+				res.status(returncode.ret);
+
+			newline = returncode.lf ?? true;
+			message = returncode.msg ?? "";
+			returncode = returncode.ret ?? 200;
+		}
+		else if (arguments.length)
+			res.status(returncode);
+
+		(newline ? console.log : process.stdout.write)(`${req.ipport} requesting ${
+			["HEAD", "OPTIONS"].includes(req.method) ? "the" : "to"
+		} ${req.method} '${req.url}' over ${req.protocol} from ${req.referrer}; ${message}${
+			message === "" ? "" : ", "
+		}sending ${returncode}`)
+
+		return this
 	}
-}
+
+	res.failurePage = function failurePage(msg = "Invalid Request", linkHome = true) {
+		try {
+			return fs.readFileSync("./public/error-template.html", "utf-8")
+				.replace("{method}", req.method)
+				.replace("{location}", req.url)
+				.replace("{message}", msg)
+				.replace("{link}", linkHome ? '<a href="/">return to homepage</a>' : "")
+		} catch {
+			return msg ?? "Invalid Request";
+		}
+	}
+
+	res.sendFailurePage = function sendFailurePage() {
+		res.setHeader("Content-Type", "text/html")
+		res.send( res.failurePage(...arguments) )
+		return this
+	}
+
+	next()
+})
 
 
-app.get("/", (req, res) => {
-	console.log(`${req.ip}:${req.connection.remotePort} requesting '/' over ${req.protocol}; use '/index.html', sending 303`)
+app.get(/\/(home|index)?$/, (req, res) => {
+	res.logRequest(303, "use '/index.html'")
 
 	res.redirect(303, "/index.html")
 })
 
-for (let loc of allowedGetLocations)
-	app.get(loc, (req, res) => {
-		console.log(`${req.ip}:${req.connection.remotePort} requesting '${loc}' over ${req.protocol}; sending 200`)
+// simplify the requests
+app.get(/\/(upload|download|delete)$/, (req, res) => {
 
-		res.status(200)
-		res.sendFile(__dirname + "/public" + loc)
+	res.logRequest(303, `use '${req.url + ".html"}'`)
+
+	res.redirect(303, req.url + ".html")
+})
+
+
+for (let loc of allowedGetAddressLocations)
+	app.get(loc, (req, res) => {
+		res.logRequest(200)
+
+		res.sendFile(loc, {root: ROOT})
 	})
 
-app.get("*", (req, res) => {
-	console.log(`${req.ip}:${req.connection.remotePort} requesting '${req.url}' over ${req.protocol}; sending 404`)
+for (let loc of allowedGetMiscellaneousLocations)
+	app.get(loc, (req, res) => {
 
+		if (req.directAddress) {
+			res.logRequest(403)
+			res.sendFailurePage("403 Forbidden")
+
+			return
+		}
+
+		res.logRequest(200)
+		res.sendFile(loc, {root: ROOT})
+	})
+
+
+app.post("/upload.html", (req, res) => {
+	if (!allowedOrigins.map(e => e + "/upload.html").concat(
+		allowedOrigins.map(e => e.replace(/:\d+$/, "") + "/upload.html")
+	).includes( req.referrer )) {
+		res.logRequest(404)
+		res.sendFailurePage("404 Not Found")
+
+		return
+	}
+
+	// store 
+
+	res.logRequest(200)
 	res.setHeader("Content-Type", "text/html")
+
+	res.sendFile("results.html", {root: ROOT})
+})
+
+app.get("*", (req, res) => {
+	res.logRequest(404)
+
 	res.status(404)
-	res.send( failurePage("GET", req, "404 Not Found") )
+	res.sendFailurePage("404 Not Found")
 })
 
 app.options("*", (req, res) => {
-	console.log(`${req.ip}:${req.connection.remotePort} requesting options over ${req.protocol}; sending 200`)
+	res.logRequest(200)
 
 	res.setHeader("Allow", allowedMethods)
 	res.setHeader("Content-Type", "text/plain")
 
-	res.status(200)
-
 	res.send(allowedGetLocationsString)
 })
 
+
+
 app.all("*", (req, res) => {
-	console.log(`${req.ip}:${req.connection.remotePort} requesting invalid method ${req.method} over ${req.protocol}; closing connection and sending 405`)
+	res.logRequest(405, "invalid method, closing connection")
 
 	res.setHeader("Content-Type", "text/plain")
 	res.setHeader("Connection", "close")
@@ -110,6 +200,8 @@ app.all("*", (req, res) => {
 const
 	httpServer = http.createServer(app),
 	httpsServer = https.createServer(credentials, app)
+
+console.log("Routers created")
 
 httpServer .listen(HTTP_PORT, IP, () =>
 	console.log(`Server is running at http://${IP}:${ HTTP_PORT}`) )
