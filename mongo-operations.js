0d5d6a3d61b45169d3aef85a60a704c36d67d2c9
Daniel Janusch <djanusch2007@gmail.com>
2023-10-23 06:27:29 -0700

    change mongo operation to use async functions

--- mongo-operations.js
+++ mongo-operations.js
@@ -6,118 +6,100 @@ export const url = "mongodb://127.0.0.1:27017/"
 // all these functions could be async functions
 
 export function connect() {
+	// returns a promise
 	return MongoClient.connect(url)
 }
 
-export function add(element) {
-	return connect().then(async client => {
-		const db = client.db("File-Sharing")
-		const main = db.collection("main")
-		const secondary = db.collection("user-keys")
-		const entries = Object.entries(element)[0]
+export async function add(element) {
+	const client = await connect()
+	const db = client.db("File-Sharing")
+	const main = db.collection("main")
+	const secondary = db.collection("user-keys")
+	const entries = Object.entries(element)[0]
 
-		await Promise.all([
-			// only use the secondary database if there is a user-friendly id
-			entries[1].userFriendlyId == null ||
-				secondary.insertOne({
-					[entries[1].userFriendlyId]: entries[0]
-				}),
-			main.insertOne(element)
-		])
+	await Promise.all([
+		// only use the secondary database if there is a user-friendly id
+		entries[1].userFriendlyId == null ||
+			secondary.insertOne({
+				[entries[1].userFriendlyId]: entries[0]
+			}),
+		main.insertOne(element)
+	])
 
-		client.close()
-	}).catch(err =>
-		console.log("error fetching MongoDB resoureces", err)
-	)
+	client.close()
 }
+export async function update(element) {
+	const client = await connect()
+	const db = client.db("File-Sharing")
+	const main = db.collection("main")
+	const fileDigest = Object.keys(element)[0]
 
-export function update(element) {
-	return connect().then(async client => {
-		const db = client.db("File-Sharing")
-		const main = db.collection("main")
+	const query = {
+		[fileDigest]: { $exists: true }
+	}
 
-		const query = {
-			[fileDigest]: { $exists: true }
-		}
+	const updates = { [fileDigest]: {
+		projectedDeleteTime : element[fileDigest].projectedDeleteTime,
+		usesRemaining       : element[fileDigest].usesRemaining,
+		originalName        : element[fileDigest].originalName,
+	} }
 
-		const updates = {
-			projectedDeleteTime : element[fileDigest].projectedDeleteTime,
-			usesRemaining       : element[fileDigest].usesRemaining,
-			originalName        : element[fileDigest].originalName,
-		}
+	// you can go up in sensitivity, but not down.
+	// this really only affects deletion.
+	if (element[fileDigest].sensitive)
+		updates[fileDigest].sensitive = true
 
-		// you can go up in sensitivity, but not down
-		if (element[fileDigest].sensitive)
-			updates.sensitive = true
+	await collection.updateOne(query, { $set: updates })
 
-		const fileDigest = Object.keys(element)[0]
+	// the secondary collection shouldn't need to be changed...
+	// so we don't need to update it here
+	// The file digest is inherant to the identity of the file,
+	// and you can't change the user-friendly id
 
-		await collection.updateOne(query, { $set: updates })
-
-		// the secondary collection shouldn't have changed
-
-		client.close()
-	}).catch(err =>
-		console.log("error fetching MongoDB resoureces:", err)
-	)
+	client.close()
 }
+export async function get(key) {
+	const client = await connect()
+	const db = client.db("File-Sharing")
+	const main = db.collection("main")
 
-export function get(key) {
-	return connect().then(async client => {
-		const db = client.db("File-Sharing")
-		const main = db.collection("main")
+	let ret = await main.findOne({
+		[key]: { $exists: true }
+	})
 
-		// TODO: there might be an error here
-		let ret = await main.findOne({
-			[key]: { $exists: true }
-		})
+	client.close()
 
-		client.close()
-
-		return ret
-	}).catch(err =>
-		console.log("error fetching MongoDB resoureces", err)
-	)
+	return ret
 }
+export async function remove(key) {
+	const client = await connect()
+	const db = client.db("File-Sharing")
+	const main = db.collection("main")
+	const secondary = db.collection("user-keys")
+	const values = Object.values(await get(key))[0]
 
-export function remove(key) {
-	return connect().then(async client => {
-		const db = client.db("File-Sharing")
-		const main = db.collection("main")
-		const secondary = db.collection("user-keys")
-		const values = Object.values( get(key) )[0]
-
+	await Promise.all([
 		// only remove from the secondary database if it is there
 		values.userFriendlyId == null ||
 			secondary.deleteOne({
 				[values.userFriendlyId]: { $exists: true }
-			})
-
-		// delete the file
-		fs.exists(values.relativeFileLocation) &&
-			fs.rm(values.relativeFileLocation)
-
-		await main.deleteOne({
+			}),
+		fs.existsSync(values.relativeFileLocation) &&
+			fs.rm(values.relativeFileLocation),
+		main.deleteOne({
 			[key]: { $exists: true }
 		})
+	])
 
-		client.close()
-	}).catch(err =>
-		console.log("error fetching MongoDB resoureces", err)
-	)
+	client.close()
 }
+export async function getAll() {
+	const client = await connect()
+	const db = client.db("File-Sharing")
+	const main = db.collection("main")
+	const docs = await main.find({}).toArray()
 
+	client.close()
 
-export function getAll() {
-	return connect().then(async client => {
-		const db = client.db("File-Sharing")
-		const main = db.collection("main")
-		const docs = await main.find({}).toArray()
-
-		client.close()
-
-		return docs
-	}).catch(err =>
-		console.log("error fetching MongoDB resoureces", err)
-	)
+	return docs
 }
